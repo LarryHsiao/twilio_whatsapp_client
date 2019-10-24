d24309b354946a8bb9c424513381116de9008142
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-10-24 14:25:59 +0800

    Endpoints

--- src/main/java/com/larryhsiao/twillio_whatsapp_client/Main.java
+++ src/main/java/com/larryhsiao/twillio_whatsapp_client/Main.java
@@ -1,12 +1,20 @@
 package com.larryhsiao.twillio_whatsapp_client;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
 import org.takes.facets.fork.FkMethods;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
 import org.takes.http.Exit;
 import org.takes.http.FtBasic;
+import org.takes.rq.RqPrint;
+import org.takes.rs.RsEmpty;
 import org.takes.tk.TkFixed;
-import org.takes.tk.TkWithType;
 
 import java.io.IOException;
 
@@ -14,12 +22,52 @@ import java.io.IOException;
  * Entry point of this sample.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        Options options = new Options();
+        options.addOption("p", "port", true, "Specify the port to run.");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        if (cmd.hasOption("p")) {
+            port = Integer.parseInt(cmd.getOptionValue("p"));
+        }
+
         new FtBasic(
-                new TkFork(
-                        new FkMethods("GET", new TkFixed("Hello world."))
+            new TkFork(
+                new FkRegex(
+                    "/twilio/callback/message",
+                    new TkFork(
+                        new FkMethods(
+                            "POST",
+                            new Take() {
+                                @Override
+                                public Response act(Request req) throws IOException {
+                                    System.out.println(new RqPrint(req).printHead());
+                                    System.out.println(new RqPrint(req).printBody());
+                                    return new RsEmpty();
+                                }
+                            }
+                        )
+                    )
                 ),
-                8080
+                new FkRegex(
+                    "/twilio/callback/status",
+                    new TkFork(
+                        new FkMethods(
+                            "POST",
+                            new Take() {
+                                @Override
+                                public Response act(Request req) throws IOException {
+                                    System.out.println(new RqPrint(req).printHead());
+                                    System.out.println(new RqPrint(req).printBody());
+                                    return new RsEmpty();
+                                }
+                            }
+                        )
+                    )
+                )
+            ),
+            port
         ).start(Exit.NEVER);
     }
 }
